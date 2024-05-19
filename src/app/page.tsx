5fa85b5a9fe033d8d661bf9135541330d5bd2fc6
JaviParMu <mrjavistinson@gmail.com>
2024-05-20 01:16:29 +0200

    testing server problem fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default async function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
-      {/* <FullPageLoader />
-      <Dashboard />
+      <FullPageLoader />
+      {/* <Dashboard />
       <Toaster /> */}
     </div>
   );
