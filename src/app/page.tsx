bc84b537cbd1d01af9fe5d938e57ef92fa25d204
JaviParMu <mrjavistinson@gmail.com>
2024-05-20 01:14:50 +0200

    testing server problem fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import { Toaster } from 'sonner';
 export default async function Home() {
   return (
     <div className="flex min-h-screen flex-col">
-      {/* <Navbar />
-      <FullPageLoader />
+      <Navbar />
+      {/* <FullPageLoader />
       <Dashboard />
       <Toaster /> */}
     </div>
