6840cbe337c5fdafd90089a7c742a8c7b802dca2
JaviParMu <mrjavistinson@gmail.com>
2024-05-20 01:04:16 +0200

    fix: remove tooltip provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,14 @@
 import { Dashboard } from '@/components/dashboard';
 import { FullPageLoader } from '@/components/full-page-loader';
 import Navbar from '@/components/navbar';
-import { TooltipProvider } from '@/components/ui/tooltip';
-import { Suspense } from 'react';
 import { Toaster } from 'sonner';
 
 export default async function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
-      <Suspense
-        fallback={
-          <main className="flex flex-1 min-h-0">
-            <aside className="hidden w-[300px] border-r lg:flex"></aside>
-          </main>
-        }
-      >
-        <FullPageLoader />
-        <TooltipProvider>
-          <Dashboard />
-        </TooltipProvider>
-      </Suspense>
+      <FullPageLoader />
+      <Dashboard />
       <Toaster />
     </div>
   );
