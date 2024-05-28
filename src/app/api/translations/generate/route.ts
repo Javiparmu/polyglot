8c94d5f4d7af63192245aed2c58cb6f9303911b9
JaviParMu <javiparmu@gmail.com>
2024-05-28 13:11:17 +0200

    fix: max duration functions

--- src/app/api/translations/generate/route.ts
+++ src/app/api/translations/generate/route.ts
@@ -4,7 +4,6 @@ import { NextResponse } from 'next/server';
 import OpenAI from 'openai';
 import { ChatCompletionMessageParam } from 'openai/resources/index.mjs';
 
-export const maxDuration = 30;
 export const dynamic = 'force-dynamic';
 
 export async function POST(request: Request) {
