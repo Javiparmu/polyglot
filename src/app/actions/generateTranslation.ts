9a4ed95835cdb8f74b904549bb7691f3d1f387f0
JaviParMu <javiparmu@gmail.com>
2024-05-28 12:35:13 +0200

    fix: generate translations

--- src/app/actions/generateTranslation.ts
+++ src/app/actions/generateTranslation.ts
@@ -1,67 +1,26 @@
 'use server';
 
 import { Config, defaultConfig } from '@/lib/config';
-import { generateTranslationIntruction } from '@/lib/openAi';
-import { S3Service } from '@/lib/s3';
 import { revalidateTag } from 'next/cache';
 import { cookies } from 'next/headers';
-import OpenAI from 'openai';
-import { ChatCompletionMessageParam } from 'openai/resources/index.mjs';
 
-export const maxDuration = 60;
-
-const instructionMessage: ChatCompletionMessageParam = {
-  role: 'system',
-  content: generateTranslationIntruction,
-};
-
-export async function generateTranslation(
+export const generateTranslation = async (
   oldLanguage: string,
   newLanguage: string,
   translation: string,
   options?: { context?: string; translateKeys?: boolean },
-): Promise<void> {
+): Promise<void> => {
   const cookiesConfig = cookies().get('config')?.value;
   const config: Config = cookiesConfig ? JSON.parse(cookiesConfig) : defaultConfig;
 
-  const openai = new OpenAI({
-    apiKey: config.openai.apiKey ?? '',
-    project: config.openai.projectId ?? '',
+  await fetch(process.env.BASE_URL + '/api/translations/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ oldLanguage, newLanguage, translation, options, config }),
+    cache: 'no-cache',
   });
 
-  const s3Service = new S3Service(config);
-  const translationData = JSON.parse(await s3Service.getObject(`${oldLanguage}/${translation}.json`));
-
-  let response: OpenAI.Chat.Completions.ChatCompletion;
-
-  try {
-    response = await openai.chat.completions.create({
-      model: 'gpt-4o',
-      response_format: { type: 'json_object' },
-      messages: [
-        instructionMessage,
-        {
-          role: 'user',
-          content: JSON.stringify({
-            oldLanguage,
-            newLanguage,
-            translation: translationData,
-            options,
-          }),
-        },
-      ],
-    });
-  } catch (error) {
-    throw new Error('Translation failed');
-  }
-
-  const responseMessage = response.choices[0].message.content;
-
-  if (!responseMessage) {
-    throw new Error('Translation failed');
-  }
-
-  await s3Service.putObject(`${newLanguage}/${translation}.json`, responseMessage);
-
   revalidateTag('translations');
-}
+};
---
+++ src/app/api/translations/generate/route.ts
@@ -0,0 +1,57 @@
+import { generateTranslationIntruction } from '@/lib/openAi';
+import { S3Service } from '@/lib/s3';
+import { NextResponse } from 'next/server';
+import OpenAI from 'openai';
+import { ChatCompletionMessageParam } from 'openai/resources/index.mjs';
+
+export const maxDuration = 60;
+
+const instructionMessage: ChatCompletionMessageParam = {
+  role: 'system',
+  content: generateTranslationIntruction,
+};
+
+export async function POST(request: Request) {
+  const { oldLanguage, newLanguage, translation, options, config } = await request.json();
+
+  const openai = new OpenAI({
+    apiKey: config.openai.apiKey ?? '',
+    project: config.openai.projectId ?? '',
+  });
+
+  const s3Service = new S3Service(config);
+  const translationData = JSON.parse(await s3Service.getObject(`${oldLanguage}/${translation}.json`));
+
+  let response: OpenAI.Chat.Completions.ChatCompletion;
+
+  try {
+    response = await openai.chat.completions.create({
+      model: 'gpt-4o',
+      response_format: { type: 'json_object' },
+      messages: [
+        instructionMessage,
+        {
+          role: 'user',
+          content: JSON.stringify({
+            oldLanguage,
+            newLanguage,
+            translation: translationData,
+            options,
+          }),
+        },
+      ],
+    });
+  } catch (error) {
+    throw new Error('Translation failed');
+  }
+
+  const responseMessage = response.choices[0].message.content;
+
+  if (!responseMessage) {
+    throw new Error('Translation failed');
+  }
+
+  await s3Service.putObject(`${newLanguage}/${translation}.json`, responseMessage);
+
+  return new NextResponse('Translation generated successfully', { status: 200 });
+}
